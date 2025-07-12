9b215d7deb7099876f9539d27edc8b1e4e9bbbeb
unknown <tmdduf785@naver.com>
2025-07-12 22:52:16 +0900

    startsettings

---
+++ community-mvp/src/services/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG6x7KDAHHkanpBV4SyyHNYeCj-EM7IJw",
+  authDomain: "communitymvp-68d46.firebaseapp.com",
+  projectId: "communitymvp-68d46",
+  storageBucket: "communitymvp-68d46.firebasestorage.app",
+  messagingSenderId: "796462450162",
+  appId: "1:796462450162:web:cb932d9bd08b3b52be576b",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
