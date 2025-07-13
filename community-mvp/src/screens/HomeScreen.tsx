9f404973229990133d879c129dfea33dbdca7025
unknown <tmdduf785@naver.com>
2025-07-13 22:22:08 +0900

    feat: FlatList

--- community-mvp/src/screens/HomeScreen.tsx
+++ community-mvp/src/screens/HomeScreen.tsx
@@ -1,13 +1,36 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { auth } from "../services/firebase";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { auth, db } from "../services/firebase";
 import { signOut } from "firebase/auth";
 import { RootStackParamList } from "../navigation/type";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
+
 export default function HomeScreen({ navigation }: Props) {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("createdAt", "desc"));
+
+    const unsubscribe = onSnapshot(q, snapshot => {
+      const data = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...(doc.data() as Omit<Post, "id">),
+      }));
+      setPosts(data);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   const handleLogout = async () => {
     await signOut(auth);
     navigation.replace("Login");
@@ -15,13 +38,24 @@ export default function HomeScreen({ navigation }: Props) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>환영합니다!</Text>
-      <Text style={styles.subtitle}>
-        지금은 홈 화면입니다. 앞으로 여기서 커뮤니티 기능이 구현될 거예요.
-      </Text>
+      <Button title="로그아웃" onPress={handleLogout} />
+      {/* <Text style={styles.subtitle}>
+        홈 화면
+      </Text> */}
       <Button title="글쓰기" onPress={() => navigation.navigate("Write")} />
 
-      <Button title="로그아웃" onPress={handleLogout} />
+
+      <FlatList
+        data={posts}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.post}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text>{item.content}</Text>
+          </View>
+        )}
+      />
+
     </View>
   );
 }
@@ -30,4 +64,5 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center", padding: 20 },
   title: { fontSize: 24, fontWeight: "bold", marginBottom: 10 },
   subtitle: { fontSize: 16, textAlign: "center", marginBottom: 20 },
+  post: { padding: 16, borderBottomWidth: 1, borderColor: "#ddd" },
 });
